e5d15bdf5ad4357ad905d9c17b6dcc5e02fa1c27
parimalmahindrakar <parimalm4653@gmail.com>
2022-05-09 21:46:56 +0530

    Added the Customer ADD operation

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,41 @@ class MainController < ApplicationController
 
     before_action :authenticate_user!
 
-
     def index
+        @customer = Customer.all()
     end
+
+
+    def create
+        @customer = Customer.new
+        
+        if request.post?
+            @customer = Customer.new(customer_params)
+            if @customer.save
+                redirect_to root_path, notice: "Successfully created account !"
+            else
+                render :new
+            end
+        end 
+
+    end
+
+
+    private
+    def customer_params 
+        params.require(:customer).permit(:name, :email, :phone, :address)
+    end
+
+
+
+    def show
+        @customer = Customer.find(params[:id])
+    end
+
+
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root "main#index"
 
+  get "/add-customer", to: "main#create"
+  post "/add-customer", to: "main#create"
+  get "/show-customer/:id", to: "main#show"
+
+
+
   
   
 
