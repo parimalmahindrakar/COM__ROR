15a8d21cf58c6e36493ab0de648e71b1c92aa675
parimalmahindrakar <parimalm4653@gmail.com>
2022-05-10 09:46:02 +0530

    added validation and styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,6 +7,11 @@ class MainController < ApplicationController
     end
 
 
+    def new
+        @customer = Customer.new
+    end
+
+
     def create
         @customer = Customer.new
         
@@ -15,7 +20,7 @@ class MainController < ApplicationController
             if @customer.save
                 redirect_to root_path, notice: "Successfully created account !"
             else
-                render :new
+                render 'new'
             end
         end 
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ActiveRecord::Base
+    validates :name, length: { minimum: 10 }
+    validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Enter the valid email address."}
+    validates :phone, presence: true, format: {with: /\A[7-9][0-9]{9}\z/, message:"Enter the valid phone number."}
+    validates :address, length: { minimum: 10 }
+
 end
